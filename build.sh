898f1f1695ee8f2a4a4c358545ebf772e3022a1d
Dylan <dylanninin@gmail.com>
2023-07-02 01:31:51 +0800

    chore: tags

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 docker build -f Dockerfile -t dylanninin/django:latest .
-docker push dylanninin/django:latest:latest
+docker push dylanninin/django:latest
+
+docker build -f Dockerfile-3.2 -t dylanninin/django:3.2 .
+docker push dylanninin/django:3.2
+
+docker build -f Dockerfile-4.0 -t dylanninin/django:4.0 .
+docker push dylanninin/django:4.0
